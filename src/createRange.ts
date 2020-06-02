c41a866ff6a3247fc4b72788005967f18fd7822b
Morris Allison III <morrisallison@users.noreply.github.com>
2020-06-02 09:14:32 -0700

    fix: support version ranges without operators

--- src/createRange.ts
+++ src/createRange.ts
@@ -1,8 +1,9 @@
-import * as semverUtils from 'semver-utils';
+import * as semverUtils from "semver-utils";
 
 export function createRange(oldRange: string, newVersion: string) {
   const parsedRange = semverUtils.parseRange(oldRange);
+  const operator = parsedRange[0].operator || "";
   const parsedVersion = semverUtils.parse(newVersion);
 
-  return `${parsedRange[0].operator}${parsedVersion.toString()}`;
+  return `${operator}${parsedVersion.toString()}`;
 }
--- test/unit/updatePackages.spec.js
+++ test/unit/updatePackages.spec.js
@@ -13,11 +13,13 @@ const logs = require("../../src/logs");
 describe("upgradePackages", () => {
   let dependencyName;
   let dependentMetas;
+  let newDesignations;
   let newRanges;
   let options;
 
   beforeEach(() => {
     dependencyName = "foo";
+
     dependentMetas = [
       {
         name: "bar",
@@ -32,35 +34,60 @@ describe("upgradePackages", () => {
         devDependencies: {
           foo: "^0.1.3"
         }
+      },
+      {
+        private: true,
+        name: "boom",
+        version: "12345.1.4",
+        peerDependencies: {
+          foo: "0.1.677-beta"
+        }
+      },
+      {
+        name: "pow",
+        version: "1.1.4",
+        dependencies: {
+          foo: "~0.1.0-beta"
+        }
       }
     ];
-    newRanges = [
-      ">=0.2.0",
-      "^0.2.0"
+
+    newRanges = [">=0.2.0", "^0.2.0", "0.2.0", "~0.2.0"];
+
+    newDesignations = [
+      "dependencies",
+      "devDependencies",
+      "peerDependencies",
+      "dependencies"
     ];
+
+    const mockDataCount =
+      dependentMetas.length + newRanges.length + newDesignations.length;
+    const hasCompleteMockData = mockDataCount % 3 === 0;
+
+    if (!hasCompleteMockData) {
+      throw new Error("Missing mock data.");
+    }
+
     options = {
       logger,
       dryRun: true,
-      packageInfos: [
-        {
-          location: "/random/path",
-          meta: dependentMetas[0]
-        },
-        {
-          location: "/whoa/buddy",
-          meta: dependentMetas[1]
-        }
-      ],
+      packageInfos: dependentMetas.map(dependentMeta => ({
+        location: `/path/to${dependentMeta.name}`,
+        meta: dependentMeta
+      })),
+
       workingPackageMeta: {
         name: dependencyName,
         version: "0.2.0"
       }
     };
 
-    td.when(jsonFilePlusMock.exports("/random/path/package.json"))
-      .thenResolve(jsonFilePlusMock.file);
-    td.when(jsonFilePlusMock.exports("/whoa/buddy/package.json"))
-      .thenResolve(jsonFilePlusMock.file);
+    for (const dependentMeta of dependentMetas) {
+      td.when(
+        jsonFilePlusMock.exports(`/path/to${dependentMeta.name}/package.json`)
+      ).thenResolve(jsonFilePlusMock.file);
+    }
   });
 
   afterEach(td.reset);
@@ -68,31 +95,27 @@ describe("upgradePackages", () => {
   it("upgrades the dependency version range in each given package", async () => {
     await upgradePackages(options);
 
-    td.verify(
-      jsonFilePlusMock.file.set({
-        dependencies: {
-          foo: newRanges[0]
-        }
-      })
-    );
-    td.verify(
-      jsonFilePlusMock.file.set({
-        devDependencies: {
-          foo: newRanges[1]
-        }
-      })
-    );
+    newRanges.forEach((newRange, i) => {
+      td.verify(
+        jsonFilePlusMock.file.set({
+          [newDesignations[i]]: {
+            foo: newRange
+          }
+        })
+      );
+    });
   });
 
   it("logs upgrades", async () => {
     await upgradePackages(options);
 
-    td.verify(
-      options.logger.log(logs.packageUpgraded(dependentMetas[0], dependencyName, newRanges[0]))
-    );
-    td.verify(
-      options.logger.log(logs.packageUpgraded(dependentMetas[1], dependencyName, newRanges[1]))
-    );
+    dependentMetas.forEach((dependentMeta, i) => {
+      td.verify(
+        options.logger.log(
+          logs.packageUpgraded(dependentMeta, dependencyName, newRanges[i])
+        )
+      );
+    });
   });
 
   it("doesn't write to the filesystem", async () => {
@@ -107,9 +130,7 @@ describe("upgradePackages", () => {
   it("logs the dry run status", async () => {
     await upgradePackages(options);
 
-    td.verify(
-      options.logger.log(logs.upgradeDryRun())
-    );
+    td.verify(options.logger.log(logs.upgradeDryRun()));
   });
 
   context("when dry run is disabled", () => {
